fcdcfe3e82f26c5c9fe72a30f0b7bfa559bcbfe9
Henry Miller <mail.henrym+git@gmail.com>
2022-02-24 10:36:16 -0500

    commit tree 42932b8dbbaef241f813383fc87a081bae1bf93e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
-end
+  attr_reader :a, :b, :c;
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a==b && b==c 
+      :equilateral
+    elsif a==b || a==c || b==c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  def sides_greater_than_zero?
+    [a,b,c].all?(&:positive?)
+  end
+
+  def validate_triangle_inequality?
+    a+b>c && a+c>b && b+c >a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && validate_triangle_inequality?
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "The given triangle is not valid"
+    end
+  end
+
+end 
